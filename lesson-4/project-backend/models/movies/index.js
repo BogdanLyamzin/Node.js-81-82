e147de3057b51f8d79fc50c5cdbb4cd5efaa565e
unknown <bogdan.lyamzin.d@gmail.com>
2023-09-28 05:10:04 +0300

    finish lesson 4

---
+++ lesson-4/project-backend/models/movies/index.js
@@ -0,0 +1,62 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const moviesPath = path.resolve("models", "movies", "movies.json");
+// const moviesPath = path.join(__dirname, "movies.json");
+
+const updateMovie = movies => fs.writeFile(moviesPath, JSON.stringify(movies, null, 2));
+
+export const getAllMovies = async()=> {
+    const buffer = await fs.readFile(moviesPath);
+    /*
+    const readFile = path => {
+        const file = read(path);
+        if(!file) {
+            throw new Error()
+        }
+    }
+    */
+    return JSON.parse(buffer);
+}
+
+export const getMovieById = async id => {
+    const movies = await getAllMovies();
+    const result = movies.find(item => item.id === id);
+    return result || null;
+}
+
+export const addMovie = async({title, director}) => {
+    const movies = await getAllMovies();
+    const newMovie = {
+        id: nanoid(),
+        title,
+        director,
+    };
+    movies.push(newMovie);
+    await updateMovie(movies);
+    return newMovie;
+}
+
+export const updateMovieById = async(id, {title, director}) => {
+    const movies = await getAllMovies();
+    const index = movies.findIndex(item => item.id === id);
+    if(index === -1) {
+        return null;
+    }
+    movies[index] = {id, title, director};
+    await updateMovie(movies);
+    return movies[index];
+}
+
+export const deleteMovieById = async id => {
+    const movies = await getAllMovies();
+    const index = movies.findIndex(item => item.id === id);
+    if(index === -1) {
+        return null;
+    }
+    
+    const [result] = movies.splice(index, 1);
+    await updateMovie(movies);
+    return result;
+}
